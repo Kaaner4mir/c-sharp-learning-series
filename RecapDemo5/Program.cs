b95832a29fa02696daea34ee9a51b9b653a62591
Emirhan Kaaner <kaaner4mir@gmail.com>
2026-03-10 22:27:01 +0300

    feat: add shapes

---
+++ RecapDemo5/Program.cs
@@ -0,0 +1,74 @@
+﻿namespace RecapDemo5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Sola dayalı dik üçgen
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    for (int j = 0; j <= i; j++)
+            //    {
+            //        Console.Write("#");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+
+            #region Ters üçgen
+
+            //for (int i = 10; i > 0; i--)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("#");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+
+            #region Sağa dayalı dik üçgen
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    for (int j = 10 - 1; j > i; j--)
+            //    {
+            //        Console.Write(" ");
+            //    }
+
+            //    for (int k = 0; k <= i; k++)
+            //    {
+            //        Console.Write("#");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+
+            #region Üçgen
+
+            //for (int i = 0; i < 7; i++)
+            //{
+            //    {
+            //        for (int k = i; k < 7; k++)
+            //        {
+            //            Console.Write(" ");
+            //        }
+            //        for (int x = 0; x <= i; x++)
+            //        {
+            //            Console.Write("#");
+            //            Console.Write(" ");
+            //        }
+            //        Console.WriteLine();
+            //    }
+            //}
+
+            #endregion
+        }
+    }
+}
+
+
