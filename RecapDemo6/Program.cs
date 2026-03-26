4f83f68dc924500d159fd21a136721e0bc0dda0c
Emirhan Kaaner <kaaner4mir@gmail.com>
2026-03-26 13:35:14 +0300

    feat: add recap demos

---
+++ RecapDemo6/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace RecapDemo6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isPrime = true;
+
+            Console.Write("Bir sayı girin: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number < 2)
+            {
+                isPrime = false;
+            }
+            else
+            {
+                for (int i = 2; i <= Math.Sqrt(number); i++)
+                {
+                    if (number % i == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+            }
+
+            if (isPrime)
+            {
+                Console.WriteLine($"{number} bir asal sayıdır.");
+            }
+            else
+            {
+                Console.WriteLine($"{number} bir asal sayı değildir.");
+            }
+        }
+    }
+}
---
+++ RecapDemo7/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace RecapDemo7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int correctNumber = rnd.Next(1, 101);
+
+            int attempt = 7;
+
+            Console.WriteLine("7 tahmin hakkın var");
+            do
+            {
+                Console.Write("\n1 ile 100 arasında bir sayı tahmin edin: ");
+                int number = Convert.ToInt32(Console.ReadLine());
+
+                if (number > correctNumber)
+                {
+                    Console.WriteLine($"Daha küçük bir sayı tahmin et! (Kalan tahmin hakkı: {attempt})");
+                    attempt--;
+                }
+                else if (number < correctNumber)
+                {
+                    Console.WriteLine($"Daha büyük bir sayı tahmin et! (Kalan tahmin hakkı: {attempt})");
+                    attempt--;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"\nDoğru! Sayı: {correctNumber}\n");
+                    Console.ResetColor();
+                    break;
+                }
+            }
+            while (attempt > 0);
+
+            if (attempt == 0)
+            {
+                Console.WriteLine("\nKaybettiniz!\n" +
+                                  $"Doğru sayı: {correctNumber}");
+
+            }
+
+        }
+    }
+}
