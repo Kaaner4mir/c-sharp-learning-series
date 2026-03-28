0ec22d281486a1ad6c5bc22866f535d649e8c61e
Emirhan Kaaner <kaaner4mir@gmail.com>
2026-03-28 22:56:02 +0300

    feat: add recap demo

---
+++ RecapDemo8/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace RecapDemo8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Birinci sayıyı girin: ");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("İkinci sayıyı girin: ");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+
+            int ebob = 1;
+            int minNumber = Math.Min(number1, number2);
+
+            for (int i = minNumber; minNumber > 0; i--)
+            {
+                if (number1 % i == 0 && number2 % i == 0)
+                {
+                    ebob = i;
+                    break;
+                }
+            }
+
+            int ekok = (number1 * number2) / ebob;
+
+            Console.WriteLine($"Ebob: {ebob}\nEkok: {ekok}");
+        }
+    }
+}
